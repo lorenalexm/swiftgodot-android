a34688f27a0002ec1883de3fc3c94bc7ccbcb43b
Alex Loren <lorenalexm@gmail.com>
2025-02-11 10:17:32 -0800

    Changed Swift version

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0.3
 import PackageDescription
 
 let package = Package(
-    name: "AndroidDemo",
+	name: "AndroidDemo",
 	platforms: [.macOS(.v14)],
-    products: [
-        .library(name: "AndroidDemo", type: .dynamic, targets: ["AndroidDemo"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
-    ],
-    targets: [
-        .target(
-            name: "AndroidDemo",
-            dependencies: ["SwiftGodot"],
+	products: [
+		.library(name: "AndroidDemo", type: .dynamic, targets: ["AndroidDemo"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
+	],
+	targets: [
+		.target(
+			name: "AndroidDemo",
+			dependencies: ["SwiftGodot"],
 			plugins: [
-                .plugin(name: "EntryPointGeneratorPlugin", package: "SwiftGodot")
+				.plugin(name: "EntryPointGeneratorPlugin", package: "SwiftGodot")
             ]
 		)
 	]
